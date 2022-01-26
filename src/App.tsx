afae0401fde07fcd9c9116ef5c3da87f2d4f197e
Adan Abbi <adanweli@hotmail.com>
2022-01-26 14:12:04 -0800

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [coins, setCoins] = React.useState([]);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const results = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets" +
+          "?vs_currency=btc&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      ).then((response) => response.json());
+
+      setCoins(results);
+    };
+
+    if (coins.length === 0) {
+      getData();
+    }
+  }, [coins]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {Array.isArray(coins) &&
+        coins.map((coin: any) => <p key={coin.id}> {coin.name}</p>)}
     </div>
   );
 }
